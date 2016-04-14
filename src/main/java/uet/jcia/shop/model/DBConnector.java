02b3a1264033997c82224baa11bc6de72885b47e
hieusonson9x <hieusonson9x@gmail.com>
2016-04-14 23:27:30 +0700

    complete Accountmanager.java

--- src/main/java/uet/jcia/shop/model/DBConnector.java
+++ src/main/java/uet/jcia/shop/model/DBConnector.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class DBConnector {
 	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://locahost:3306/shop";
-	private static final String USERNAME = "bqcuong"; 
-	private static final String PASSWORD = "buiquangcuong";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/shop";
+	private static final String USERNAME = "root"; 
+	private static final String PASSWORD = "26031812";
 	private static Connection con = null;
 	private static DBConnector connector = new DBConnector();
 	
@@ -15,6 +15,7 @@ public class DBConnector {
 		try {
 			Class.forName(JDBC_DRIVER).newInstance();
 			con = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
 			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -33,5 +34,7 @@ public class DBConnector {
 	public static DBConnector getInstance() { return connector; }
 	
 	public Connection getConnection() { return con; }
+	
+	
 
 }
