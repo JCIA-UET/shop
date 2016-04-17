56e77ecd57d9b6998e835e66b9020062179707e8
dinhtienloc <dinhtienloc1996@gmail.com>
2016-04-17 17:40:15 +0700

    update and finish Statist, add addDetailToOrder method

--- src/main/java/uet/jcia/shop/model/OrderManager.java
+++ src/main/java/uet/jcia/shop/model/OrderManager.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -12,12 +13,16 @@ import java.util.List;
 public class OrderManager implements ItemManager {
 	private Connection conn = null;
 	
-	private final String GET_ORDER_BY_ID_QUERY  = "SELECT * FROM shop.order WHERE orderID=?";
-	private final String GET_ALL_ORDERS_QUERY    = "SELECT * FROM shop.order";
-	private final String GET_DETAILS_BY_ORDER_ID = "SELECT * FROM shop.orderdetail WHERE orderID=?";
-	private final String DELETE_ORDER_BY_ID      = "DELETE FROM shop.order WHERE orderID=?";
-	private final String ADD_NEW_ORDER 				 = "INSERT INTO shop.order (customerID) VALUES(?)";
-	private final String GET_TOP_SELLING_PRODUCT = "SELECT product.productID, SUM(orderdetail.Quantity) AS Quantity FROM orderdetail INNER JOIN product ON product.productID = orderdetail.productID GROUP BY orderdetail.productID ORDER BY Quantity DESC LIMIT ?";
+	private final String GET_ORDER_BY_ID_QUERY  		= "SELECT * FROM shop.order WHERE orderID=?";
+	private final String GET_ALL_ORDERS_QUERY   		= "SELECT * FROM shop.order";
+	private final String GET_DETAILS_BY_ORDER_ID_QUERY 	= "SELECT * FROM shop.orderdetail WHERE orderID=?";
+	private final String DELETE_ORDER_BY_ID_QUERY    	= "DELETE FROM shop.order WHERE orderID=?";
+	private final String ADD_NEW_ORDER_QUERY	 		= "INSERT INTO shop.order (customerID) VALUES(?)";
+	private final String ADD_NEW_ORDERDETAIL_QUERY		= "INSERT INTO shop.orderdetail (orderID, productID, quantity, orderDate) VALUES (?,?,?,now())";
+	private final String GET_TOP_SELLING_PRODUCT_QUERY 	= "SELECT product.productID, SUM(orderdetail.Quantity) AS Quantity FROM orderdetail INNER JOIN product ON product.productID = orderdetail.productID GROUP BY orderdetail.productID ORDER BY Quantity DESC LIMIT ?";
+	private final String GET_REVENUE_IN_DAY_QUERY 		= "SELECT DATE(orderDate) AS Date, SUM(orderdetail.quantity * product.price) AS Revenue FROM shop.orderdetail LEFT JOIN product ON product.productID = orderdetail.productID WHERE DATE(orderDate)=?";
+	private final String GET_REVENUE_IN_MONTH_QUERY		= "SELECT MONTH(DATE(orderDate)) as Month, SUM(orderdetail.quantity * product.price) AS Revenue FROM shop.orderdetail LEFT JOIN product ON product.productID = orderdetail.productID WHERE DATE(orderDate) BETWEEN ? AND ?";
+	private final String GET_TOTAL_REVENUE_QUERY		= "SELECT SUM(orderdetail.quantity * product.price) as Revenue FROM shop.orderdetail LEFT JOIN product ON product.productID = orderdetail.productID";
 	
 	/**
 	 * get order 
@@ -111,7 +116,7 @@ public class OrderManager implements ItemManager {
 		conn = dbConnector.createConnection();
 		
 		try {
-			PreparedStatement stt = conn.prepareStatement(DELETE_ORDER_BY_ID);
+			PreparedStatement stt = conn.prepareStatement(DELETE_ORDER_BY_ID_QUERY);
 			stt.setInt(1, id);
 			
 			stt.executeUpdate();
@@ -135,17 +140,17 @@ public class OrderManager implements ItemManager {
 	
 	
 	/**
-	 * get all products in an order
+	 * get all details in an order
 	 * 
-	 * @param id                    id of order want to get details
-	 * @return List<OrderDetails>   list of item in order
+	 * @param id                    id of order want to see details
+	 * @return List<OrderDetails>   list of orderdetail in order
 	 */
 	public List<OrderDetails> GetDetailsById(int id) {
 		conn = dbConnector.createConnection();
 		List<OrderDetails> list = new ArrayList<>();
 		
 		try {
-			PreparedStatement stt = conn.prepareStatement(GET_DETAILS_BY_ORDER_ID);
+			PreparedStatement stt = conn.prepareStatement(GET_DETAILS_BY_ORDER_ID_QUERY);
 			stt.setInt(1,id);
 			ResultSet rs = stt.executeQuery();
 			while(rs.next()) {
@@ -161,17 +166,54 @@ public class OrderManager implements ItemManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return list;
 	}
 	
+	public boolean addDetailToOrder(OrderDetails detail) {
+		conn = dbConnector.createConnection();
+		
+		try {
+			PreparedStatement stt = conn.prepareStatement(ADD_NEW_ORDERDETAIL_QUERY);
+			stt.setInt(1, detail.getOrderId());
+			stt.setInt(2, detail.getProductId());
+			stt.setInt(3, detail.getQuantity());
+			
+			stt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * add a new order into database
+	 * @param newItem			order want to add
+	 * @return					true if the addition is successful . Otherwise return false.
+	 */
 	@Override
 	public boolean addItem(Item newItem) {
 		if(!(newItem instanceof Order)) return false;
 		conn = dbConnector.createConnection();
 		
 		try {
-			PreparedStatement stt = conn.prepareStatement(ADD_NEW_ORDER);
+			PreparedStatement stt = conn.prepareStatement(ADD_NEW_ORDER_QUERY);
 			Order currentItem = (Order)newItem;
 			int customerID = currentItem.getCustomerId();
 			stt.setInt(1, customerID);
@@ -183,13 +225,20 @@ public class OrderManager implements ItemManager {
 			e.printStackTrace();
 			return false;
 		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	/**
-	 * get all products in an order
-	 * 
-	 * @param id                    id of order want to get details
-	 * @return List<OrderDetails>   list of item in order
+	 * get the list of best-selling products
+	 * @param n                    	the number of products want to list
+	 * @return List<OrderDetails>   list of products
 	 */
 	public List<Item> getTopSellingProduct(int n) {
 		conn = dbConnector.createConnection();
@@ -197,7 +246,7 @@ public class OrderManager implements ItemManager {
 		ProductManager pm = new ProductManager();
 		
 		try {
-			PreparedStatement stt = conn.prepareStatement(GET_TOP_SELLING_PRODUCT);
+			PreparedStatement stt = conn.prepareStatement(GET_TOP_SELLING_PRODUCT_QUERY);
 			stt.setInt(1, n);
 			ResultSet rs = stt.executeQuery();
 			while(rs.next()) {
@@ -212,13 +261,108 @@ public class OrderManager implements ItemManager {
 		}
 		return null;
 	}
-	
+	/**
+	 * compute revenue in a day
+	 * @param date				the day we want to see revenue
+	 * @return revenue			revenue of this day
+	 */
 	public double calDailyRevenue(Date date) {
-		return 0;
+		conn = dbConnector.createConnection();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String szDate = sdf.format(date);
+		
+		try {
+			PreparedStatement stt = conn.prepareStatement(GET_REVENUE_IN_DAY_QUERY);
+			stt.setString(1, szDate);
+			
+			ResultSet rs = stt.executeQuery();
+			rs.next();
+			double revenue = rs.getDouble("Revenue");
+			return revenue;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
+	/**
+	 * compute revenue in a month
+	 * @param month				the month we want to see revenue
+	 * @return revenue			revenue of this month
+	 */
+	
+	public double calMonthRevenue(int month) {
+		int lastDay;
+		String szDate;
+		
+		conn = dbConnector.createConnection();
+	
+		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) lastDay = 31;
+		else if(month == 2) lastDay = 28;
+		else lastDay = 30;
+		
+		try {
+			PreparedStatement stt = conn.prepareStatement(GET_REVENUE_IN_MONTH_QUERY);
+			szDate = String.format("2016-" + month + "-" + 1);
+			stt.setString(1, szDate);
+			szDate = String.format("2016-" + month + "-" + lastDay);
+			stt.setString(2, szDate);
+			
+			ResultSet rs = stt.executeQuery();
+			rs.next();
+			double revenue = rs.getDouble("Revenue");
+			return revenue;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * compute total revenue
+	 * @param month
+	 * @return
+	 */
 	public double calTotalRevenue() {
-		return 0;
+		conn = dbConnector.createConnection();
+		try {
+			PreparedStatement stt = conn.prepareStatement(GET_TOTAL_REVENUE_QUERY);
+			ResultSet rs = stt.executeQuery();
+			rs.next();
+			
+			double revenue = rs.getDouble("Revenue");
+			return revenue;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
--- src/test/java/uet/jcia/shop/test/model/OrderManagerTest.java
+++ src/test/java/uet/jcia/shop/test/model/OrderManagerTest.java
@@ -2,6 +2,9 @@ package uet.jcia.shop.test.model;
 
 import uet.jcia.shop.model.*;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /**
  * @author dinht_000
  *
@@ -13,20 +16,30 @@ public class OrderManagerTest {
 		Item newOrder = null;
 		Item newOrderDetails = null;
 		
+		// Date format
+		/*
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String sDate = sdf.format(date);
+		System.out.println(sDate);
+		*/
+		
 		//System.out.println("Get order by id:");
 		//orderManager.getItemById(1);
-		System.out.println("Get All Order:");
-		orderManager.getAllItems();
+		//System.out.println("Get All Order:");
+		//orderManager.getAllItems();
 		//System.out.println("Get details:");
 		//System.out.println(orderManager.GetDetailsById(1));
-		System.out.println("Remove Order");
-		orderManager.removeItemById(1);
-		System.out.println("Get All Order:");
-		orderManager.getAllItems();
+		//System.out.println("Remove Order");
+		//orderManager.removeItemById(1);
+		//System.out.println("Get All Order:");
+		//orderManager.getAllItems();
 		
-		newOrder = new Order(1);
-		orderManager.addItem(newOrder);
-		
-		orderManager.getTopSellingProduct(10);
+		//newOrder = new Order(1);
+		//orderManager.addItem(newOrder);
+		//System.out.println(orderManager.calDailyRevenue(new Date()));
+		//System.out.println(orderManager.calMonthRevenue(4));
+		//System.out.println(orderManager.calTotalRevenue());
+		//orderManager.getTopSellingProduct(10);
 	}
 }
